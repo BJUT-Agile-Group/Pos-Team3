aff548e2f8bf51d97a866da054c2c4ed409228e9
Hugh <hugh_77@icloud.com>
2014-12-30 21:14:37 +0800

    Add a test about discount

--- test/PosTest.java
+++ test/PosTest.java
@@ -81,4 +81,25 @@ public class PosTest {
         assertThat(actualShoppingList, is(expectedShoppingList));
 
     }
+
+    @Test
+    public void testGetCorrectShoppingListForReq2() throws Exception {
+        //given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000004","电池","个",2.00,0.8));
+
+        //when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+
+        //then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        + "名称：电池，数量：1个，单价：2.00(元)，小计：1.60(元)\n"
+                        + "----------------------\n"
+                        + "总计：1.60(元)\n"
+                        + "节省：0.40(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
 }

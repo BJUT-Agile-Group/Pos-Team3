cf0ef5ab21d408193632c94862515d22e3e14a77
梁勋· <keensting0712@icloud.com>
2015-01-02 23:16:09 +0800

    input type change
    change the variable's type

---
+++ keensting/README.md
@@ -0,0 +1,15 @@
+POS-Team 3 Project     
+=================     
+    
+This is a project made by Team 3 realizes POS.   
+Team 3 members:   
+Team Leader: 	洪鑫    
+Team Members:   
+							邓旺华   
+							吕成   
+							梁勋   
+							王晓晨   
+							胡义嘉   
+							姚炳勋   
+							黄修权   
+							张黎明   
---
+++ keensting/src/domains/ShoppingListItem.java
@@ -0,0 +1,34 @@
+package domains;
+
+/**
+ * Created by Hugh on 2014/12/29 0029.
+ */
+public class ShoppingListItem extends Item{
+    private int amount;
+    private double subTotal;
+    public ShoppingListItem(Item item) {
+        this.barCode = item.barCode;
+        this.name = item.name;
+        this.unit = item.unit;
+        this.price = item.price;
+        this.discount = item.discount;
+        this.amount = 1;
+        this.subTotal = item.price*discount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public double getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(double subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
